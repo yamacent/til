bc1eac2e099075da89e0344e543d6928eacf24db
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2019-07-06 21:24:28 +0900

    Update

--- js/IntersectionObserver/main.js
+++ js/IntersectionObserver/main.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const target = document.querySelector('.div2')
-    const observer = new IntersectionObserver(changes => {
-        console.log(changes)
+    const observer = new IntersectionObserver((entries, observer) => {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                console.log('visible')
+            } else {
+                console.log('not visible')
+            }
+        }
     })
     observer.observe(target)
 })
